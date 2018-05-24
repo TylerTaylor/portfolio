602afdd3eee36706a919b56ee5c1bbe79fe2e1a2
Tyler Taylor <vikingtyty@gmail.com>
2018-05-24 10:40:08 -0500

    Customized project route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :projects
+  resources :projects, except: [:show]
+
+  get 'project/:id', to: 'projects#show', as: 'project_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
