8fe4ad4b378b2615702b0d40e35923f6ad85a432
Tyler Taylor <vikingtyty@gmail.com>
2018-05-26 09:44:53 -0500

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ end
 end
 
 puts "6 project items created"
+
+3.times do |technology|
+    Project.last.technologies.create!(
+        name: "Technology #{technology}"
+    )
+end
+
+puts "3 technologies created"
