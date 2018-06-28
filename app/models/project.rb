dc076ea87e42cd46b4e38fdc6666d120dfa07ff8
Tyler Taylor <vikingtyty@gmail.com>
2018-06-28 10:07:54 -0500

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,8 @@ class Project < ApplicationRecord
     has_many :technologies
     accepts_nested_attributes_for :technologies,
                                   reject_if: lambda { |attrs| attrs['name'].blank? }
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
+
+    validates_presence_of :title, :body
 
     # mount_uploader tells project model it needs to call carrierwave
     # apply uploader to thumb_image/main_image, use Project Uploader
@@ -22,10 +22,4 @@ class Project < ApplicationRecord
     scope :ruby_on_rails_project_items, -> { where(subtitle: 'Ruby on Rails') }
     # end scopes
 
-    after_initialize :set_defaults
-
-    def set_defaults
-        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
 end
